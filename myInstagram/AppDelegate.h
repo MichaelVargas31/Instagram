4482df9b15a7c6549cc9b5960e04fcbf29aa6cc0
Michael Vargas <michaelvargas@fb.com>
2019-07-08 15:28:09 -0700

    Initial Commit

---
+++ myInstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myInstagram
+//
+//  Created by michaelvargas on 7/8/19.
+//  Copyright © 2019 michaelvargas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myInstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myInstagram
+//
+//  Created by michaelvargas on 7/8/19.
+//  Copyright © 2019 michaelvargas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
